f9def21fe1c671b4b19f067044a0512e98f86301
janus1809 s <jean.amaral@etu.univ-smb.fr>
2023-03-03 11:02:04 +0000

    Mon code

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
 # save this as app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def start():
+    return render_template("index.html")
+
+@app.route("/filter")
+def rules_filter():
+    return ""
+
+@app.route("/nat")
+def rules_nat():
+    return ""
+
+@app.route("/add")
+def rules_nat_add():
+    return ""
+
+@app.route("/alias")
+def alias():
+    return ""
