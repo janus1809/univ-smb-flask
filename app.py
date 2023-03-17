85a33f99bde6ed05245905d3cb117a9ac2079944
janus1809 s <jean.amaral@etu.univ-smb.fr>
2023-03-17 09:03:43 +0000

    TP5 Jean AMARAL TRI1 version 1

--- app.py
+++ app.py
@@ -1,25 +1,74 @@
 # save this as app.py
-from flask import Flask, render_template
-
+import base64
+from flask import Flask, render_template, request, session
 app = Flask(__name__)
-
+app.secret_key = b'JanusLePetit'
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    if 'authentification' in session:
+        if session['authentification'] == 1 :
+            return render_template("index.html")
+        else:  
+            return render_template("login.html")
+    else:  
+        return render_template("login.html")
 
 @app.route("/filter")
 def rules_filter():
-    return ""
+    if 'authentification' in session:
+        if session['authentification'] == 1 :
+            return render_template("rules_filter.html")
+        else:  
+            return render_template("login.html")
+    else:  
+        return render_template("login.html")
+    
 
 @app.route("/nat")
 def rules_nat():
-    return ""
+    if 'authentification' in session:
+        if session['authentification'] == 1 :
+            return render_template("rules_nat.html")
+        else:  
+            return render_template("login.html")
+    else:  
+        return render_template("login.html")
+    
 
 @app.route("/add")
 def rules_nat_add():
-    return ""
+    if 'authentification' in session:
+        if session['authentification'] == 1 :
+            return render_template("rules_nat.html")
+        else:  
+            return render_template("login.html")
+    else:  
+        return render_template("login.html")
 
 @app.route("/alias")
 def alias():
-    return ""
+    if 'authentification' in session:
+        if session['authentification'] == 1 :
+            return render_template("alias.html")
+        else:  
+            return render_template("login.html")
+    else:  
+        return render_template("login.html")
+    
+
+@app.route("/login", methods=['POST'])
+def login():
+    login = request.form['username']
+    password = request.form['password']
+    hach = login +':'+ password
+    hach = base64.b64encode(hach.encode('utf-8'))
+    with open("mdp.txt", "r") as f:
+        mdpbase = f.read()
+    print(mdpbase)
+    print(hach.decode('utf-8'))
+    if mdpbase == hach.decode('utf-8') :
+        session['authentification'] = 1
+        return render_template('index.html')
+    else :
+        return render_template('login.html')
